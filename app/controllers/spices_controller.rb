f6875bfe0857402e4901953644ba69ce1e8c58a0
OnanThairu <thairuonan@gmail.com>
2022-09-28 07:42:39 +0300

    Add update route and action.

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -11,7 +11,18 @@ class SpicesController < ApplicationController
     spice = Spice.create(spice_params)
     render json: spice
   end
-  
+
+  # PATCH /spices/:id
+  def update
+    spice = Spice.find_by_id(params[:id])
+    if spice
+      spice.update(spice_params)
+      render json: spice
+    else
+      render json: { error: "Spice not found" }
+    end
+  end
+
 
   private
 
