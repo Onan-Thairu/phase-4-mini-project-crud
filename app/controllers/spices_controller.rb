c9c8bd4a30b38dd5889978623f401532f1bacb07
OnanThairu <thairuonan@gmail.com>
2022-09-28 07:32:20 +0300

    Add spices POST route and action.

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,6 +1,21 @@
 class SpicesController < ApplicationController
+
+  # GET /spices
   def index
     spices = Spice.all
     render json: spices
+  end
+
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice
+  end
+  
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
   end
 end
